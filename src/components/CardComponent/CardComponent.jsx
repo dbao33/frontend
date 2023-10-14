bab28ef173bdfb15529d89292647f8bffdbd5c78
dbao33 <dbao.le332@gmail.com>
2023-10-14 12:23:12 +0700

    tạo card cho product

---
+++ src/components/CardComponent/CardComponent.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Card } from 'antd'
+import {
+    StyleNameProduct, WrapperReportText, WrapperStyleTextSell,
+    WrapperPriceText, WrapperPriceDiscountText, WrapperCardStyle
+} from './style'
+import { StarFilled } from '@ant-design/icons'
+import logo from '../../assets/images/logo.png'
+
+const CardComponent = () => {
+    const {Meta}=Card
+    return (
+      <WrapperCardStyle
+            hoverable
+            style={{ width: 200 }}
+            headStyle={{ width: '200px', height: '200px' }}
+            bodyStyle= {{ padding: '10px'}}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            
+        >
+            <img
+                src={logo}
+                alt="logo"
+                style={{
+                    top: -1,
+                    left: -1,
+                    borderTopLeftRadius: '3px',
+                    position: 'absolute',
+                    height: '14px',
+                    width: '68px'
+                }}
+            />
+            <StyleNameProduct>Lenovo</StyleNameProduct>
+            <WrapperReportText>
+                <span style={{ marginRight: '4px'}}>
+                <span>4.5</span> <StarFilled  style={{ fontSize: '12px', color: 'rgb(253, 216, 54)' }} />
+                </span>
+                <WrapperStyleTextSell>  |  Da ban 1000+ </WrapperStyleTextSell>
+            </WrapperReportText>
+            <WrapperPriceText>19.000.000d
+                <WrapperPriceDiscountText style={{ marginLeft: '4px'}}>
+                -15%
+                </WrapperPriceDiscountText>
+            </WrapperPriceText>
+        </WrapperCardStyle>
+  )
+}
+
+export default CardComponent
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import TypeProduct from '../../components/TypeProduct/TypeProduct'
 import { WrapperTypeProduct } from './style'
 import SliderComponent from '../../components/SliderComponent/SliderComponent.jsx'
-import slider_2 from "../../assets/images/slider_2.webp"
-import slider_1 from "../../assets/images/slider_1.png"
-import slider_4 from "../../assets/images/slider_4.jpg"
-import slider_5 from "../../assets/images/slider_5.jpg"
-
+import slider_2 from '../../assets/images/slider_2.webp'
+import slider_1 from '../../assets/images/slider_1.png'
+import slider_4 from '../../assets/images/slider_4.jpg'
+import slider_5 from '../../assets/images/slider_5.jpg'
+import CardComponent from '../../components/CardComponent/CardComponent'
 const HomePage = () => {
     const arr = ['ASUS', 'HP', 'DELL']
     return (
@@ -21,9 +21,13 @@ const HomePage = () => {
                 </WrapperTypeProduct>
                 
             </div>
-            <div id="container" style={{ backgroundColor: '#efefef', padding: '0 120px', height: '1000px' }}>
+            <div id="container" style={{ backgroundColor: '#efefef', padding: '0 120px', height: '274px' }}>
                 <SliderComponent arrImages={[slider_1, slider_2, slider_4, slider_5]}/>
             </div>
+            <div style={{marginTop: '20px',display: 'flex', alignItems: 'center', gap:'20px'}}>
+                <CardComponent />
+            </div>
+            
         </>
     )
 }
