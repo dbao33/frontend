238b52a13b960a4383fbe0ecd1bbcd7f867a83e8
dbao33 <dbao.le332@gmail.com>
2023-10-16 14:18:18 +0700

    tích hợp react query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { routes } from './routes/indexRoutes'
 import DefaultComponent from './components/DefaultComponent/DefaultComponent'
-
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
 function App() {
+
+  // useEffect(() => {
+  //   fetchApi()
+  // }, [])
+
+  console.log('env', process.env.REACT_API_URL_BE)
+  const fetchApi = async () => {
+    const respone = await axios.get(`http://localhost:5000/v1/api/product/get-all-products`)
+    // const respone = await axios.get(`${process.env.REACT_API_URL_BE}/product/get-all-products`)
+
+    return respone.data
+  }
+  const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi })
+  console.log('query', query)
   return (
 
     < div >
